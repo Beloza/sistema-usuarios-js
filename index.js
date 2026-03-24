13e4da854e3d0ee98e9a7587a1b87d223d150d2d
unknown <antonioacbss@gmail.com>
2026-03-24 08:03:51 -0300

    Adicionando funcao de cadastro de usuario

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 let usuarios = [];
 
 console.log("Siatema iniciado");
+
+function cadastrarUsuario(nome, email, senha) {
+
+    let novoUsuario = {
+        nome: nome,
+        email: email,
+        senha: senha
+    };
+    usuarios.push(novoUsuario);
+
+    console.log("Usuario cadastrado");
+}
+cadastrarUsuario("Antonio", "antonio@email", "1234");
+console.log(usuarios);
